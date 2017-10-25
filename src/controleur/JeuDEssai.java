3454d3658dbc47534f41a76022ff8a8b62150f20
Juluto <julian.mayer94@gmail.com>
2017-10-25 13:37:47 +0200

    encore plus d'adresse :dancer:

--- src/controleur/JeuDEssai.java
+++ src/controleur/JeuDEssai.java
@@ -25,6 +25,16 @@ public class JeuDEssai {
 		listAdresse.add(new Adresse(0, 2, "rue des Macaques", 68700, "Wattwiller"));
 		listAdresse.add(new Adresse(0, 182, "rue de Rivoli", 75000, "Paris"));
 		listAdresse.add(new Adresse(0, 25, "rue Saint-Pierre", 13005, "Marseille"));
+		listAdresse.add(new Adresse(0, 1, "rue Saint Dizier", 49440, "Angrie"));
+		listAdresse.add(new Adresse(0, 78, "rue des Projets", 18600, "Vereaux"));
+		listAdresse.add(new Adresse(0, 85, "rue de la Marmotte", 71990, "Saint-Prix"));
+		listAdresse.add(new Adresse(0, 24, "rue Sergent Blandan", 40280, "Bretagne-de-Marsan"));
+		listAdresse.add(new Adresse(0, 36, "quaies des Orfevres", 11220, "Tournissan"));
+		listAdresse.add(new Adresse(0, 89, "rue des Fleurs", 15190, "Saint-Amandin"));
+		listAdresse.add(new Adresse(0, 47, "rue de la Place Carnot", 32160, "Tasque"));
+		listAdresse.add(new Adresse(0, 12, "rue de Blizzard", 39800, "Barretaine"));
+		listAdresse.add(new Adresse(0, 25, "rue des arbres", 68490, "Hombourg"));
+		listAdresse.add(new Adresse(0, 25, "rue de toto", 17540, "Bouhet"));
 		Iterator<Adresse> iterator = listAdresse.listIterator();
 		while (iterator.hasNext()) {
 			em.getTransaction().begin();
